85d9bd45366e43cfac8577a8645fa5843469bc43
Thito Ribeiro <thito.ribeiro@hotmail.com>
2023-01-14 10:50:21 -0300

    First custom component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpensiveItem from "./components/ExpenseItem";
+
 function App() {
     return (
       <div>
         <h2>Let's get started!</h2>
-        <p>This is also viseble!</p>
+        <ExpensiveItem></ExpensiveItem>
       </div>
     );
   }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpensiveItem() {
+    return <h2>Expense Item</h2>
+}
+
+export default ExpensiveItem;
